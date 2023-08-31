5339ef24e5081f750a60ebd59fa842bef91fe03e
André <AndreR@ppedv.de>
2023-08-31 15:42:40 +0200

    Datenbank direkt

---
+++ EntityFramework/HalloDb/HalloDb/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Data.SqlClient;
+
+Console.WriteLine("Hello DB");
+
+var conString = "Server=(localdb)\\mssqllocaldb;Database=NORTHWND;Trusted_Connection=True;";
+//var conString = "Server=.;Database=NORTHWIND;Trusted_Connection=True;TrustServerCertificate=True;";
+
+var con = new SqlConnection(conString);
+con.Open();
+
+var cmd = con.CreateCommand();
+cmd.CommandText = "SELECT COUNT(*) FROM Employees";
+var rowCount = cmd.ExecuteScalar();
+Console.WriteLine($"{rowCount} Employees gefunden");
+
+
+cmd = con.CreateCommand();
+cmd.CommandText = "SELECT * FROM Employees";
+var reader = cmd.ExecuteReader();
+while (reader.Read())
+{
+    var vorname = reader.GetString(reader.GetOrdinal("FirstName"));
+    var gebDatum = reader.GetDateTime(reader.GetOrdinal("BirthDate"));
+    Console.WriteLine($"{vorname} {gebDatum:d}");
+}
